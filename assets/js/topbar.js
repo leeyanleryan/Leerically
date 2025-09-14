3542d9ff220847cdd5be41e93d88fbb9145221c1
Ryan Lee <leeyanleryan.21@gmail.com>
2025-09-14 22:53:09 +0800

    Setup React

--- assets/js/topbar.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const menuButton = document.querySelector('.menu-btn');
-  const primaryNav = document.querySelector('.primary-nav');
-  menuButton.classList.toggle('open');
-  primaryNav.classList.toggle('open');
-}
--- sites/aimer/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: default
-title: Aimer
-permalink: /aimer/
----
-
-{% include media-overview.html section_title="Lyrics Translations" header="Aimer" %}
