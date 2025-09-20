4e0f3f7e123c6a4530361650d137e59730f341a4
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-20 15:29:14 +0800

    Include site.webmanifest file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
       </Head>
