70c8744329d57bd9f9e7c4bbf91bf070a8ff383e
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-20 16:09:12 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,10 @@ import '../src/app/css/main.css'
 import '../src/app/css/Hero.css'
 import Topbar from '../src/app/tsx/Topbar'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Leerically</title>
-        <meta name="description" content="A platform for understanding song lyrics." />
-      </Head>
       <Topbar />
       <Component {...pageProps} />
     </>
