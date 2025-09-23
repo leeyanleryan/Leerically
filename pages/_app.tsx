04cf27e1ec3b8a8c4e7c305ec750795fe93934c2
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-23 23:10:30 +0800

    Remove @next/third-parties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import '../src/app/css/main.css'
 import '../src/app/css/Hero.css'
 import Topbar from '../src/app/tsx/Topbar'
 import type { AppProps } from 'next/app'
-import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics gaId="G-5V3FP1TLB4" />
       <Topbar />
       <Component {...pageProps} />
     </>
