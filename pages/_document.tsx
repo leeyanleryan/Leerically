0aed2beffa4a2393e6533e00e6534e541adb3e13
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-23 23:32:54 +0800

    Test other gtag script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,10 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"></script>
-        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} /> */}
-        <Script id="google-gtag" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4" />
-        <Script id="google-gtag-init" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"></script>
+        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} />
+        {/* <Script id="google-gtag" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4" />
+        <Script id="google-gtag-init" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} /> */}
       </Head>
       <body>
         <Main />
