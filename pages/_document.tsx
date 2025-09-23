e2735af36c84b449b5db0470ec30146fb0f2d967
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-23 21:49:40 +0800

    Switch to next/script for gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,8 +9,8 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"></script>
-        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4" />
+        <Script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`,}} />
       </Head>
       <body>
         <Main />
