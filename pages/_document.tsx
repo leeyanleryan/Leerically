d8a757bd24c6d6a025e2b128ae26e727b8af24e8
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-23 21:34:04 +0800

    Use @next/third-parties for gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -10,18 +9,6 @@ export default function Document() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
       </Head>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-5V3FP1TLB4');
-        `}
-      </Script>
       <body>
         <Main />
         <NextScript />
