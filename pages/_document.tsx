e741d796e0378dd91380f6b2a1ae75a9a6baa994
leeyanleryan <leeyanleryan.21@gmail.com>
2025-09-23 21:45:12 +0800

    Change gtag script to one line

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,8 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-5V3FP1TLB4');
-            `,
-          }}
-        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5V3FP1TLB4"></script>
+        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5V3FP1TLB4');`}} />
       </Head>
       <body>
         <Main />
